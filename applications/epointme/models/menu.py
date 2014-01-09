9eb649407f2de34568306789e26a8d5986298bc8
Trevor Overman <troverman@gmail.com>
2014-01-09 04:44:31 -0500

    new repo
    1

---
+++ applications/epointme/models/menu.py
@@ -0,0 +1,20 @@
+################################################################
+####menu.py#####################################################
+################################################################
+
+################################
+####title#######################
+################################  
+if request.function == 'index':
+    response.title = 'epoint.me'
+else:
+    response.title = request.functionresponse.subtitle = ''
+response.logo = A(B('epoint.me'), _class="brand",_href="http://www.epoint.me/")
+
+################################
+####meta########################
+################################ 
+response.meta.author = 'Trevor Overman'
+response.meta.description = 'epoint.me'
+response.meta.keywords = 'epoint, value, market, giving, transparent'
+response.meta.generator = 'epoint.me'
